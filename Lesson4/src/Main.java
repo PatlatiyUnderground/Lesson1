4eb3d29e7c7585046c3c84efcc4e8a0e021affef
PatlatiyUnderground <patlatiyunderground@gmail.com>
2025-05-21 19:36:08 +0300

    Add Lesson4

---
+++ Lesson4/src/Main.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//7. Задание особой сложности *.  Дописать аналоги нашего калькулятора так, чтобы они могли считать сумму, разность и частное двух чисел.
+//8. Задание особой сложности **. Сделать так, чтобы все наши калькуляторы могли работать как с целыми числами, так и с дробными.
+
+//
+//        byte a = 1;
+//                short b = 2;
+//                        char catKid = 'm';
+//                                int doIt = 4;
+//                                        long popIt = 5;
+//
+//char str = 'a';
+//char str2 = 'b';
+//
+//int bon = 10;
+//
+//float bin = 10.2F;
+//
+//
+//        System.out.print(a);
+//        System.out.print(b);
+//        System.out.print(catKid);
+//        System.out.print(doIt);
+//        System.out.print(popIt);
+//        System.out.print(str);
+//        System.out.print(str2);
+//        System.out.print(bon);
+//        System.out.print(bin);
+//
+//        System.out.println("\t\t Калькулятор сложения чисел");
+//        System.out.println("Введите первое число:");
+//        double a = new Scanner(System.in).nextDouble();
+//        System.out.println("Введите второе число:");
+//        double b = new Scanner(System.in).nextDouble();
+//
+//        System.out.println(a + " + " + b + " = " + (a + b));
+
+//
+//        System.out.println("\t\t Калькулятор вычитания чисел");
+//        System.out.println("Введите первое число:");
+//        int a = new Scanner(System.in).nextInt();
+//        System.out.println("Введите второе число:");
+//        int b = new Scanner(System.in).nextInt();
+//
+//        System.out.println(a + " - " + b + " = " + (a - b));
+
+//        System.out.println("\t\t Калькулятор умножения чисел");
+//        System.out.println("Введите первое число:");
+//        int a = new Scanner(System.in).nextInt();
+//        System.out.println("Введите второе число:");
+//        int b = new Scanner(System.in).nextInt();
+//
+//        System.out.println(a + " * " + b + " = " + (a * b));
+//
+//        System.out.println("\t\t Калькулятор деления чисел");
+//        System.out.println("Введите первое число:");
+//        int a = new Scanner(System.in).nextInt();
+//        System.out.println("Введите второе число:");
+//        int b = new Scanner(System.in).nextInt();
+//
+//        System.out.println(a + " / " + b + " = " + (a / b));
+
+//           (F = C * 1.8 + 32).
+//        System.out.println("\tЦельсия в Фарингейт");
+//        System.out.println("\tВведите значение температуры");
+//        double c = new Scanner(System.in).nextDouble();
+//        System.out.println("F" + "=" + (c * 1.8 + 32));
+
+//        System.out.println("\t ***Перевод рубля в валюту***");
+//        System.out.println("\tВведите кол-во рублей:");
+//        double r = new Scanner(System.in).nextDouble();
+//        System.out.println("\tD = " + (0.012 * r)+ "\tE =" + (0.011 * r));
+
+
+
+
+
+
+    }
+}
