98a221231ab6e041464ef19584b9bb057d747aeb
PatlatiyUnderground <patlatiyunderground@gmail.com>
2025-05-21 19:19:14 +0300

    Add Lesson1.3

---
+++ Lesson1.3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("**************************\n");
+        System.out.print("****      Визитка     ****\n");
+        System.out.print("****    Name: \"Богдан\"****\n");
+        System.out.print("****  Skype: my skype ****\n");
+        System.out.print("**************************");
+    }
+}
